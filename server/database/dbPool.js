367ef84183b8caf811e3f5b8fbb0d845764c9992
Leandro Murgo <leandrouni01@gmail.com>
2021-06-13 18:36:56 -0300

    feat(server database) create dbPool to access connections to the database

---
+++ server/database/dbPool.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const { 
+  DB_POOL_LIMIT, 
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_DATABASE 
+} = require('../config/dev');
+//exported instance of createpool so it works as a singleton due to module caching of node.js
+module.exports = mysql.createPool({
+  connectionLimit: DB_POOL_LIMIT,
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE
+});
