ded464b4a359b3ef35c6bf140a79eea294ad2bcf
Leandro Murgo <leandrouni01@gmail.com>
2021-06-12 22:42:21 -0300

    feat(server index.js) add midleware for parsing body of requests

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//Midlewares
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.listen(PORT, () => {
   console.log("Server is listening on port: ", PORT);
 });
