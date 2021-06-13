cf277a17147fd16a355c012b6cf26c5f40c82c1e
Leandro Murgo <leandrouni01@gmail.com>
2021-06-13 19:03:48 -0300

    feat(server database) create dbOperations and add basic structure

---
+++ server/database/dbOperations.js
@@ -0,0 +1,20 @@
+exports.insert = (model, data) => {
+
+}
+
+exports.find = (model, data) => {
+
+}
+
+exports.findOne = (model, data) => {
+
+}
+
+exports.update = (model, data) => {
+
+}
+
+exports.delete = (model, data) => {
+
+}
+
