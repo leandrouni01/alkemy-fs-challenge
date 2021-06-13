2b2447619ec643c0d2c07388963eb9278f153335
Leandro Murgo <leandrouni01@gmail.com>
2021-06-13 19:55:20 -0300

    feat(server dbOperations) add functionality to insert method of dbOpertations

--- server/database/dbOperations.js
+++ server/database/dbOperations.js
@@ -1,5 +1,24 @@
-exports.insert = (model, data) => {
+const pool = require('./dbPool');
 
+exports.insert = (model, data) => {
+  const { table, keys } = model;
+  const query =
+    `INSERT INTO ${table} (${keys.map((key) => key.key).join(", ")}) VALUES (${keys.map((key) => "?").join(", ")})`;
+  
+  const insertData = keys.map(key => data[key.key]);
+
+  return new Promise((resolve, reject) => {
+    pool.getConnection(function(err, connection) {
+      if (err) reject(err);
+     
+      connection.query(query,insertData, (error, result) => {
+        connection.release();
+        
+        if (error) return reject(error);
+        resolve(result.insertId);
+      });
+    });
+  })
 }
 
 exports.find = (model, data) => {
