ba8e9824e1b3eab29ca98c7bcc6396fb2202857e
Leandro Murgo <leandrouni01@gmail.com>
2021-06-14 04:31:57 -0300

    feat(server index.js) add user routes to API

--- server/index.js
+++ server/index.js
@@ -7,6 +7,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded());
 
+//Routes
+const userRoutes = require('./routes/users');
+
+
+//API routes
+app.use('/api/v1/users', userRoutes);
+
 app.listen(PORT, () => {
   console.log("Server is listening on port: ", PORT);
 });
