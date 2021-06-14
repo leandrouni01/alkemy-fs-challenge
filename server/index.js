bdcfd944712474da53bf779aa872cc8174140ed2
Leandro Murgo <leandrouni01@gmail.com>
2021-06-14 20:14:05 -0300

    feat(server index.js) add operation routes to app

--- server/index.js
+++ server/index.js
@@ -9,10 +9,11 @@ app.use(express.urlencoded({extended: true}));
 
 //Routes
 const userRoutes = require('./routes/users');
-
+const operationRoutes = require('./routes/operations');
 
 //API routes
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/operations', operationRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is listening on port: ", PORT);
