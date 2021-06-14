c8a1568a8c53c043ac3451cf60e787beded808c4
Leandro Murgo <leandrouni01@gmail.com>
2021-06-14 10:06:00 -0300

    feat(server controllers) create operations controller and add basic structure

---
+++ server/controllers/operation.js
@@ -0,0 +1,23 @@
+const operationModel = require('../models/Operation');
+const dbOperations = require('../database/dbOperations');
+
+
+module.exports.getOperation = (req,res) =>{
+
+}
+
+module.exports.getOperationById = (req,res) =>{
+
+}
+
+module.exports.createOperation = (req,res) =>{
+
+}
+
+module.exports.updateOperation = (req,res) =>{
+
+}
+
+module.exports.deleteOperation = (req,res) =>{
+
+}
