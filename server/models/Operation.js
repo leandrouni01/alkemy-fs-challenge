080f1ab9d3962ed337e4cf24cae6b41ee0657d33
Leandro Murgo <leandrouni01@gmail.com>
2021-06-15 11:04:16 -0300

    fix(server models) change parameters of update in operation model

--- server/models/Operation.js
+++ server/models/Operation.js
@@ -3,15 +3,15 @@ const table = "operation";
 const keys = [
   { key: "id", table },
   { key: "fk_user", table },
+  { key: "type", table },
   { key: "concept", table },
   { key: "amount", table },
   { key: "date", table },
-  { key: "type", table },
   { key: "category", table },
   
 ]
 
 module.exports.find = {keys, table};
 module.exports.insert = {keys: keys.slice(1), table};
-module.exports.update = {keys: keys.slice(2), table};
+module.exports.update = {keys: keys.slice(3), table};
 module.exports.delete = {keys, table};
