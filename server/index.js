8f575f3e51d95e9c228ef382bc62f9e496856361
Leandro Murgo <leandrouni01@gmail.com>
2021-06-15 14:40:43 -0300

    feat(server index.js) add categories routes to app

--- server/index.js
+++ server/index.js
@@ -10,10 +10,12 @@ app.use(express.urlencoded({extended: true}));
 //Routes
 const userRoutes = require('./routes/users');
 const operationRoutes = require('./routes/operations');
+const categoryRoutes = require('./routes/categories');
 
 //API routes
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/operations', operationRoutes);
+app.use('/api/v1/categories', categoryRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is listening on port: ", PORT);
