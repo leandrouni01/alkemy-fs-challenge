0c92af87dfa9491862b267cac4c29b14278648a6
Leandro Murgo <leandrouni01@gmail.com>
2021-06-14 21:30:04 -0300

    feat(server controllers) add functionality to getOperation method of operation controller

--- server/controllers/operation.js
+++ server/controllers/operation.js
@@ -3,7 +3,14 @@ const dbOperations = require('../database/dbOperations');
 
 
 module.exports.getOperations = (req,res) =>{
-
+  const fk_user = res.locals.user.id;
+  dbOperations.findBy(operationModel.find, {fk_user, conditions: [{key: "fk_user"}]})
+  .then((results) => {
+    return res.status(200).send(results);
+  })
+  .catch((err) => {
+    return res.status(422).send({errors: [{ title: "Db Error", detail: err.message }] })
+  })
 }
 
 module.exports.getOperationById = (req,res) =>{
