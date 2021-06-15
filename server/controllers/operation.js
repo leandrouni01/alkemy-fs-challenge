3303875f25a18ac0ee7bd47b1f6d5be8b7cc4877
Leandro Murgo <leandrouni01@gmail.com>
2021-06-14 22:20:22 -0300

    feat(server controllers) add functionality to getOperationById method of operation controller

--- server/controllers/operation.js
+++ server/controllers/operation.js
@@ -14,7 +14,18 @@ module.exports.getOperations = (req,res) =>{
 }
 
 module.exports.getOperationById = (req,res) =>{
-
+  const userId = res.locals.user.id;
+  const { operationId } = req.params
+  dbOperations.findOne(operationModel.find, {id: operationId})
+  .then((results) => {
+    if(results.length > 0 && userId != results[0].fk_user) {
+      return res.status(402).send({errors: [{ title: "Unauthorized", detail: "Current user does not have acces to requested operation" }] });
+    }
+    return res.status(200).send(results);
+  })
+  .catch((err) => {
+    return res.status(422).send({errors: [{ title: "Db Error", detail: err.message }] })
+  })
 }
 
 module.exports.createOperation = (req,res) =>{
