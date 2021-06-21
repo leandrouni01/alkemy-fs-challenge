3d1d09e186ca25b1ddb1a08e05cdc898263ce53a
Leandro Murgo <leandrouni01@gmail.com>
2021-06-21 20:56:05 -0300

    fix(components header) change reserved word class for className in collapse button of header

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -5,8 +5,8 @@ const Header = (props) => {
   return(
     <nav className="navbar navbar-expand-md navbar-dark pb-header">
       <Link className="navbar-brand" to="/">Personal Budget</Link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
