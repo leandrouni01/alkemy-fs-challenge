89e2601fc7cac335ae287fe95b6d76691d4a6952
Leandro Murgo <leandrouni01@gmail.com>
2021-06-21 05:04:00 -0300

    feat(src components) create header component with router functionality

---
+++ src/components/shared/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = (props) => {
+  return(
+    <nav className="navbar navbar-expand-md navbar-dark pb-header">
+      <Link className="navbar-brand" to="/">Personal Budget</Link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/operations">Manage Operations</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/register">Register</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Header;
