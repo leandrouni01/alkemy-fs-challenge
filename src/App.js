66cd9d4ff66fa2e0a8ee73bef0bd9945b878d43b
Leandro Murgo <leandrouni01@gmail.com>
2021-06-21 05:05:19 -0300

    feat(src app) prepare app for use of react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from './Routes';
 
+import Header from 'components/shared/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes />
+    </Router>
   );
 }
 
