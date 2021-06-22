59f3d5d55dc3c1c94499e5158224eb02c8dda47c
Leandro Murgo <leandrouni01@gmail.com>
2021-06-21 21:29:24 -0300

    feat(src routes) add Login and Register views in paths on Routes component

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Register from 'views/Register';
+import Login from 'views/Login';
+
 const Routes = () => {
   return (
     <div className="container">
       <Switch>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
       </Switch>
     </div>
   )
