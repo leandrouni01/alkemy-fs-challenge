fde2f355a22e5b447277b175e663aaa7c3b0a253
Leandro Murgo <leandrouni01@gmail.com>
2021-06-22 12:24:15 -0300

    feat(redux store) add operations reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import auth from './reducers/auth'
+import operations from './reducers/operations'
 
 export function initStore() {
     const reducers = combineReducers({
-        auth
+        auth,
+        operations
     });
 
     const store = createStore(reducers, applyMiddleware(thunk));
