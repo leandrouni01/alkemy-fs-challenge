977a6aab6dbee9dcbec8e7d6558c314db6aebb0c
Leandro Murgo <leandrouni01@gmail.com>
2021-06-22 05:44:52 -0300

    feat(pages login) complete login page functionality

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,19 +1,46 @@
 import React from "react";
 import LoginForm from "components/forms/LoginForm";
+import { Redirect } from 'react-router-dom';
+import { withAuth } from 'providers/AuthProvider';
 
 class Login extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      shouldRedirect: false,
+      errors: []
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   handleSubmit(registerData) {
-    console.log(registerData);
+    this.props.auth.signIn(null, registerData)
+    .then(_=> {
+      this.setState({shouldRedirect: true});
+    })
+    .catch((err) => {
+      this.setState({errors: err});
+    })
   }
 
   render() {
+
+    const { errors, shouldRedirect } = this.state;
+
+    if(shouldRedirect){
+      return <Redirect to={{pathname: '/'}} />
+    }
 
     return (
       <div className="pb-form">
         <div className="row">
           <div className="col-6">
             <h1 className="page-title">Login</h1>
+            {
+              errors.length > 0 &&
+              errors.map(err => <div className="alert alert-danger" key={err.detail}>{err.detail}</div>)
+            }
             <LoginForm onSubmit={this.handleSubmit}/>
           </div>
           <div className="col-6">
@@ -27,4 +54,4 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+export default withAuth(Login);
