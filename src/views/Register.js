83bd189d696fd77955a65b7bd38cf5fc8bce36aa
Leandro Murgo <leandrouni01@gmail.com>
2021-06-22 05:03:14 -0300

    feat(pages register) implemented auth provider to complete register page functionality

--- src/views/Register.js
+++ src/views/Register.js
@@ -1,19 +1,50 @@
 import React from "react";
 import RegisterForm from "components/forms/RegisterForm";
+import { Redirect } from 'react-router-dom';
+import { registerUser } from 'actions';
+import { withAuth } from 'providers/AuthProvider';
 
 class Register extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      shouldRedirect: false,
+      errors: []
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   handleSubmit(registerData) {
-    console.log(registerData);
+    registerUser(registerData)
+    .then((res) => {
+      return this.props.auth.signIn(res.data.token);
+    })
+    .then(_=> {
+      this.setState({shouldRedirect: true});
+    })
+    .catch(err => {
+      this.setState({errors: err});
+    })
   }
 
   render() {
+
+    const { errors, shouldRedirect } = this.state;
+
+    if(shouldRedirect){
+      return <Redirect to={{pathname: '/'}} />
+    }
 
     return (
       <div className="pb-form">
         <div className="row">
           <div className="col-6">
             <h1 className="page-title">Register</h1>
+            {
+              errors.length > 0 &&
+              errors.map(err => <div className="alert alert-danger" key={err.detail}>{err.detail}</div>)
+            }
             <RegisterForm onSubmit={this.handleSubmit}/>
           </div>
           <div className="col-6">
@@ -27,4 +58,4 @@ class Register extends React.Component {
   }
 }
 
-export default Register;
+export default withAuth(Register);
