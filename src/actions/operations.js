243f7712bef21ff00a914b598c5a2dc83818321b
Leandro Murgo <leandrouni01@gmail.com>
2021-06-22 12:25:10 -0300

    feat(actions operations) create fetch and delete operation actions

---
+++ src/actions/operations.js
@@ -0,0 +1,24 @@
+import axiosService from 'services/AxiosService';
+import { extractApiErrors } from './index';
+const { pbAxios } = axiosService;
+
+export const fetchOperations = () => dispatch => {
+  dispatch({type: "REQUEST_DATA"});
+  return pbAxios.get('/operations')
+  .then((res)=> {
+    dispatch({type: "REQUEST_DATA_COMPLETE", items});
+  })
+  .catch((err)=> {
+    dispatch({type: "ERROR_OCURRED",errors: extractApiErrors(err)});
+  })
+}
+
+export const deleteOperation = (id) => dispatch => {
+  return pbAxios.delete('/operations/'+id)
+  .then((res)=> {
+    dispatch({type: "DELETE_ITEM", id});
+  })
+  .catch((err)=> {
+    dispatch({type: "ERROR_OCURRED",errors: extractApiErrors(err)});
+  })
+}
