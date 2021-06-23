8ae7301c28736038e87cbdf1971f85e9f248f841
Leandro Murgo <leandrouni01@gmail.com>
2021-06-23 16:05:42 -0300

    feat(helpers) create validator method for required exchangeableSelect

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -2,3 +2,9 @@ export const sameAs = (field, getValues, message) => (value) => {
   const compareTo = getValues()[field];
   return value === compareTo || message;
 }
+
+
+export const requiredExchangeableSelect = (field, getValues, message) => (value) => {
+  // eslint-disable-next-line
+  return getValues()[field] != "" || message;
+}
