af3193b581ab7772e94166066cdec114a489ec7b
Leandro Murgo <leandrouni01@gmail.com>
2021-06-24 17:13:47 -0300

    feat(helpers) add helpers function to get formatted date

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -1,3 +1,7 @@
 export const capitalize = (data) => {
   return typeof data === "string" && data.length > 0 ? data.charAt(0).toUpperCase() + data.slice(1) : data;
 }
+
+export const cleanDate = (date) => {
+  return date && typeof date === "string" ? date.split('T')[0] : date;
+}
