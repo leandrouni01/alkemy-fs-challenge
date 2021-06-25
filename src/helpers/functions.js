c71c57557116fb2ab408aa25e084e36b7f8ee27f
Leandro Murgo <leandrouni01@gmail.com>
2021-06-25 00:48:21 -0300

    feat(helpers) create helper function getCurrentDate

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -5,3 +5,12 @@ export const capitalize = (data) => {
 export const cleanDate = (date) => {
   return date && typeof date === "string" ? date.split('T')[0] : date;
 }
+
+export const getCurrentDate = () => {
+  const d = new Date()
+
+  return [
+    d.getFullYear(),
+    d.getMonth() < 9 ? '0' + (d.getMonth() + 1) : '' + (d.getMonth() + 1),
+    d.getDate() < 10 ? '0' + d.getDate() : '' + d.getDate()].join('-');
+}
